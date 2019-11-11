02157e2b32b00b6e4f200fb491f852945c4e5e83
Laura Spieker <ljspiek@gmail.com>
2019-11-10 19:43:37 -0600

    updated styling and navigation

---
+++ javascript.js
@@ -0,0 +1,10 @@
+const navSlide = () => {
+    const burger = document.querySelector(".hamburger");
+    const nav = document.querySelector(".nav-links");
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    });
+}
+
+navSlide();
